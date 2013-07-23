132da4238504023ce5db7dae9c6db0587b4f1f43
Daniel E. Renfer <duck@kronkltd.net>
2013-07-23 13:47:26 -0400

    dependencies

--- project.clj
+++ project.clj
@@ -2,15 +2,14 @@
   :description "Airbrake Client"
   :url "https://github.com/duck1123/clj-airbrake"
   :min-lein-version "2.0.0"
-  :dependencies [[clj-http "0.7.2"]
-                 [clj-stacktrace "0.2.5"]
-                 [ring/ring-core "1.2.0-beta3"]
-                 [org.clojure/data.zip "0.1.0"]
+  :dependencies [[clj-http "0.7.5"]
+                 [clj-stacktrace "0.2.6"]
+                 [ring/ring-core "1.2.0"]
+                 [org.clojure/data.zip "0.1.1"]
                  [org.clojure/clojure "1.5.1"]
-                 [hiccup "1.0.0"]]
+                 [hiccup "1.0.4"]]
   :profiles {:dev
-             {:dependencies [[lein-clojars "0.6.0"]
-                             [enlive "1.0.0-SNAPSHOT"]
-                             [midje "1.5-RC1"]]}}
+             {:dependencies [[enlive "1.0.0-SNAPSHOT"]
+                             [midje "1.6-alpha2"]]}}
   :plugins [[lein-midje     "3.0-beta1"]]
 )
